4117282bcd8604d32e8a501f9eecfb27c51aee8f
DiegoSouza7 <diego78523@gmail.com>
2021-03-29 18:44:28 -0300

    feat: create html and css page home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <div className={styles.container}>
+      <img src="/icon.svg" alt="Logo" />
+      <h1>
+        spacetraveling
+      </h1>
+      <p>.</p>
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import { FaRegCalendarAlt, FaUser } from 'react-icons/fa'
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +25,45 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ }: HomeProps) {
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const teste = ['1', '2', '3', '4', '5', '6', '7']
 
-//   // TODO
-// };
+  return (
+    <main className={styles.container}>
+      {teste.map(teste => (
+        <div key={teste} className={styles.content}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <div>
+              <FaRegCalendarAlt />
+                  15 Mar 2021
+                </div>
+            <div>
+              <FaUser />
+              Diego Souza
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <footer>
+        <button>
+          Carregar mais posts
+        </button>
+      </footer>
+    </main>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  // const prismic = getPrismicClient();
+  // const postsResponse = await prismic.query(TODO);
+
+  // TODO
+
+  return {
+    props: {}
+  }
+};
